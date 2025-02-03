a8a232fc52511d78b8ac0ac98c6e543950b5ead2
Mabona3 <mazenkhaled2003@gmail.com>
2025-02-03 22:17:13 +0200

    Initial Commit

---
+++ main/rfid.cpp
@@ -0,0 +1,26 @@
+#include "rfid.h"
+
+/*void printHex(byte *buffer, byte bufferSize);*/
+/**/
+/*void RFID_init() {*/
+/*  SPI.begin();*/
+/*  mfrc522.PCD_Init();*/
+/*  mfrc522.PCD_DumpVersionToSerial();*/
+/*  for (byte i = 0; i < 6; i++) {*/
+/*    key.keyByte[i] = 0xFF;*/
+/*  }*/
+/*  Serial.println(F("RFID initialized"));*/
+/*  Serial.print("Using the following key: ");*/
+/*  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);*/
+/*}*/
+/**/
+/*void printHex(byte *buffer, byte bufferSize) {*/
+/*  for (byte i = 0; i < bufferSize; i++) {*/
+/*    Serial.print(buffer[i] < 0x10 ? " 0" : " ");*/
+/*    Serial.print(buffer[i], HEX);*/
+/*  }*/
+/*}*/
+/**/
+/*void RFID_read(void *pvParameters) {*/
+/**/
+/*}*/
