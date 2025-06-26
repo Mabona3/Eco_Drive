bec22aca6c8c3014da1c2478fb35e7813921f0d5
Mabona3 <mazenkhaled2003@gmail.com>
2025-06-26 03:14:55 +0300

    formatted the code and changed the printed text to determine errors

--- main/gps.cpp
+++ main/gps.cpp
@@ -1,60 +1,32 @@
-#include <freertos/FreeRTOS.h>
 #include "gps.h"
-#include "firebase.h"
+#include "HardwareSerial.h"
+#include <freertos/FreeRTOS.h>
 
-void GPS_init() {
-  GPS_Serial.begin(9600, SERIAL_8N1, GPS_PIN_RX, GPS_PIN_TX);
-}
+void GPS_init() { GPS_Serial.begin(9600, SERIAL_8N1, GPS_PIN_RX, GPS_PIN_TX); }
 
 void GPS_read(void *pvParameters) {
   for (;;) {
-    // This is also non blocking function it just a wrapper
-    // around uart_get_buff_len which get the length of 
-    // uart buffered data.
+    Serial.println("Searching GPS...");
     if (GPS_Serial.available()) {
-      char c = GPS_Serial.read();
-      if (gps.encode(c)) {
+      Serial.println("GPS: ");
+
+      if (gps.encode(GPS_Serial.read()))
         processData();
-      }
+
+    } else {
+      Serial.println("GPS not available...");
     }
-    vTaskDelay(1000);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
 void processData() {
-  bool valid = gps.location.isValid();
   displayInfo();
-
-  if (valid && gps.date.day() != 0) {
-    Serial.println("Sending data to the firebase.");
-    FirebaseJson locationData;
-    locationData.add("lat", gps.location.lat());
-    locationData.add("lng", gps.location.lng());
-
-    FirebaseJson timeData;
-    timeData.add("hour", gps.time.hour());
-    timeData.add("minute", gps.time.minute());
-    timeData.add("second", gps.time.second());
-    timeData.add("centisecond", gps.time.centisecond());
-
-    FirebaseJson dateData;
-    dateData.add("month", gps.date.month());
-    dateData.add("day", gps.date.day());
-    dateData.add("year", gps.date.year());
-    FirebaseJson json;
-    json.add("location", locationData);
-    json.add("time", timeData);
-    json.add("date", dateData);
-    message msg = {&json, "/gps"};
-    xQueueSend(queue, &msg, 1000);
-  }
-  else {
-    Serial.println("GPS data is not sent");
-  }
+  // TODO: Update this to upload via the mqtt
 }
 
 void displayInfo() {
-  Serial.print(F("Location: ")); 
+  Serial.print(F("Location: "));
   if (gps.location.isValid()) {
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
@@ -76,16 +48,20 @@ void displayInfo() {
 
   Serial.print(F(" "));
   if (gps.time.isValid()) {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
+    if (gps.time.hour() < 10)
+      Serial.print(F("0"));
     Serial.print(gps.time.hour());
     Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
+    if (gps.time.minute() < 10)
+      Serial.print(F("0"));
     Serial.print(gps.time.minute());
     Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
+    if (gps.time.second() < 10)
+      Serial.print(F("0"));
     Serial.print(gps.time.second());
     Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    if (gps.time.centisecond() < 10)
+      Serial.print(F("0"));
     Serial.print(gps.time.centisecond());
   } else {
     Serial.print(F("INVALID"));
--- main/gps.h
+++ main/gps.h
@@ -1,18 +1,15 @@
 #ifndef GPS_H
 #define GPS_H
 
+#include "constant.h"
 #include <HardwareSerial.h>
 #include <TinyGPSPlus.h>
-#include "firebase.h"
-#include "constant.h"
 
 static HardwareSerial GPS_Serial(1);
 static TinyGPSPlus gps;
 
 void GPS_init();
-
 void GPS_read(void *pvParameters);
-
 void processData();
 void displayInfo();
 
