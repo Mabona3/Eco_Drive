bcb997253deddeaa76a87852e3f2a38f1a40030c
Mabona3 <mazenkhaled2003@gmail.com>
2025-07-01 16:38:19 +0300

    Converting the data to the string to upload it and overwrites when the queue is full

--- main/queue_prepare.cpp
+++ main/queue_prepare.cpp
@@ -1,7 +1,8 @@
 #include "queue_prepare.h"
 #include "esp32-hal.h"
+#include "freertos/projdefs.h"
 
-static QueueHandle_t mqttQueue;
+QueueHandle_t mqttQueue;
 EcoDriveData ecoDriveData;
 SemaphoreHandle_t EcoDriveMutex;
 
@@ -14,18 +15,34 @@ void QueuePrepare_task(void *pvParameters) {
   EcoDriveData snapshot;
   queueData data;
   for (;;) {
-    if (xSemaphoreTake(EcoDriveMutex, 0)) {
+    if (xSemaphoreTake(EcoDriveMutex, 0) == pdTRUE) {
       snapshot = ecoDriveData;
       xSemaphoreGive(EcoDriveMutex);
       if (snapshot.gps_valid || snapshot.obd_valid) {
-        snprintf(data.data, MQTT_MSG_MAX_LEN, "{}");
-        xQueueSend(mqttQueue, &data, 100);
+        snprintf(data.data, MQTT_MSG_MAX_LEN,
+                 "{\"rpm\": %f, \"speed\": %d, \"coolant_temp\": %d, "
+                 "\"load\": %d, \"fuel_level\": %d, \"throttle\": %d, \""
+                 "intake_temp\": %d, \"maf\": %f \"lng\": %lf, \"lat"
+                 "\": %lf \"time\": \"%d/%d/%d %d:%d:%d\"}",
+                 snapshot.obd_data.engineRPM, snapshot.obd_data.vehicleSpeed,
+                 snapshot.obd_data.coolantTemp, snapshot.obd_data.engineLoad,
+                 snapshot.obd_data.fuelLevel, snapshot.obd_data.throttlePos,
+                 snapshot.obd_data.intakeTemp, snapshot.obd_data.mafRate,
+                 snapshot.gps_lng, snapshot.gps_lat, snapshot.date.day,
+                 snapshot.date.month, snapshot.date.year, snapshot.time.hour,
+                 snapshot.time.minute, snapshot.time.second);
+
+        if (xQueueSendToBack(mqttQueue, &data, 0) != pdPASS) {
+          queueData dummy;
+          xQueueReceive(mqttQueue, &dummy, 0);
+          xQueueSendToBack(mqttQueue, &data, 0);
+        }
       } else {
         Serial.println("No Valid Data");
       }
     } else {
       Serial.println("Prepare is waiting for the mutex");
     }
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 }
--- main/queue_prepare.h
+++ main/queue_prepare.h
@@ -3,12 +3,26 @@
 
 #include <esp32-hal.h>
 
+#include "TinyGPS++.h"
 #include "carStatus.h"
 #include "constant.h"
 
 // "{"rpm": 6667.00, "speed": 300, "coolant_temp": 185, "load": 628,
 // "fuel_level": 100, "throttle": 114, "intake_temp": 157, "maf": 7.00
-// "lng": 90.123456, "lat": 90.123456}"
+// "lng": 90.123456, "lat": 90.123456 "time": "10/10/2010 12:59:59"}"
+//
+
+struct Date {
+  int year;
+  int month;
+  int day;
+};
+
+struct Time {
+  int hour;
+  int minute;
+  int second;
+};
 
 // A struct to hold data to be uploaded to the queueHandle.
 typedef struct {
@@ -20,6 +34,9 @@ typedef struct {
   double gps_lng, gps_lat;
   bool gps_valid;
 
+  struct Date date;
+  struct Time time;
+
   // OBD Data
   OBDData obd_data;
   // Valid if connected only can't validate the car data it will be.
@@ -29,6 +46,7 @@ typedef struct {
 
 extern EcoDriveData ecoDriveData;
 extern SemaphoreHandle_t EcoDriveMutex;
+extern QueueHandle_t mqttQueue;
 
 void QueuePrepare_init();
 void QueuePrepare_task(void *pvParameters);
