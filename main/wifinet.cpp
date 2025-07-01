5da35475bb5998bedf507d1deb85dc8254d980e4
Mabona3 <mazenkhaled2003@gmail.com>
2025-07-01 16:29:35 +0300

    Made Wifinet.c which has the declaration of the function

---
+++ main/wifinet.cpp
@@ -0,0 +1,11 @@
+#include "wifinet.h"
+
+void WiFinet_init() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("Connected");
+}
--- main/wifinet.h
+++ main/wifinet.h
@@ -4,14 +4,6 @@
 #include "constant.h"
 #include <WiFi.h>
 
-void WiFi_init() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println("Connected");
-}
+void WiFinet_init();
 
 #endif // WIFINET_H
